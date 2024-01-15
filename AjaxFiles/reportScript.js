29903285a996a1ed36cd97d4849f63056a004293
gauravi26 <gauravidamahe26@gmail.com>
2024-01-15 17:28:41 +0530

    manually appending script saving in a variable , appending on body

--- AjaxFiles/reportScript.js
+++ AjaxFiles/reportScript.js
@@ -1,43 +1,27 @@
-var controllerName = $("#controllerId").val();
-var actionName = $("#actionId").val();
+var customScript = document.createElement('script');
 
-// Call the function to fetch the report script
-fetchReportScript(controllerName, actionName);
 
-function fetchReportScript(controller, action) {
-    $.ajax({
-        url: 'index.php?r=effects/reportScriptMapping',
-        type: 'GET',
-        data: { controller: controller, action: action },
-        success: function (response) {
-            // Apply the fetched script
-//            applyScript(response);
 
-                $("#scriptPlaceholder").append("<script>"+response+"</script>");
-        },
-        error: function () {
-            console.error('Error fetching report script.');
-        }
-    });
-}
+//$.ajax{(
+//            
+//            
+//            
+//            
+//            
+//            )}
+customScript.type = 'text/javascript';
 
-function applyScript(script) {
-    // Create a script element
-    var customScript = document.createElement('script');
+effectScript = 
+    ` var percentageElements = document.querySelectorAll('table td:nth-child(8)');
+    percentageElements.forEach(function (element) {
+      var percentage = parseInt(element.textContent);
+      if (percentage >= 45) {
+        element.style.setProperty('color', 'green', 'important');
+      } else {
+        element.style.setProperty('color', 'red', 'important');
+      }
+    }); `;
 
-    // Set the type attribute to JavaScript
-    customScript.type = 'text/javascript';
-    customScript.setAttribute('defer', 'defer');
+console.log(effectScript);
 
-    // Set the content of the script
-    customScript.textContent = script;
-    
-    // Append the script to the head of the document
-    document.head.appendChild(customScript);
-    console.log(customScript);
-    // Add an event listener for the 'load' event to ensure the script has loaded
-    customScript.addEventListener('load', function () {
-        // Script has loaded, and you can perform actions after it has loaded.
-        console.log('Script has loaded');
-    });
-}
+$("body").append("<script>"+effectScript+"</script>");
