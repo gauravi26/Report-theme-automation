274e1d1358e77fd29e299fbf9e571911891e4944
gauravi26 <gauravidamahe26@gmail.com>
2024-01-16 14:01:51 +0530

    There was change in function call due to which the theme function was called This is final working thememing script for report

--- AjaxFiles/themeingReport.js
+++ AjaxFiles/themeingReport.js
@@ -14,7 +14,7 @@
 function fetchCssProperties(controller, action) {
     // Make the AJAX request to fetch CSS properties
     $.ajax({
-        url: 'index.php?r=effects/reportScriptMapping',
+        url: 'index.php?r=reportThemeMapping/applyThemeReport',
         type: 'GET',
         dataType: 'text', // Change the data type to 'text'
         data: { controller: controller, action: action },
